723458b9598cae46369c638f7567e9079899d5c9
Little <414929680@qq.com>
2021-01-10 12:07:56 +0800

    1 add a log 2 gradle version 3 gradle plugin version

--- app/src/main/java/com/zfang/graphicdemo/base/BaseActivity.kt
+++ app/src/main/java/com/zfang/graphicdemo/base/BaseActivity.kt
@@ -12,6 +12,7 @@ open class BaseActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: ")//add a log in onCreate
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
