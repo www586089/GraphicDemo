e552a7252bbf050cde1dd906c1c4bc32fb88bfaf
Little <414929680@qq.com>
2021-03-28 21:04:12 +0800

    测试类

--- app/src/test/java/com/zfang/graphicdemo/RentalTest.java
+++ app/src/test/java/com/zfang/graphicdemo/RentalTest.java
@@ -1,4 +1,23 @@
 package com.zfang.graphicdemo;
 
+import com.zfang.graphicdemo.refact.Customer;
+import com.zfang.graphicdemo.refact.Movie;
+import com.zfang.graphicdemo.refact.Rental;
+
 public class RentalTest {
+
+    private static RentalTest instance = new RentalTest();
+
+    public static void main(String[] args) {
+        instance.test();
+    }
+
+    private void test() {
+        Customer customer = new Customer("Jack");
+        customer.addRental(new Rental(new Movie("<童话大王>", Movie.CHILDRENS), 1));
+        customer.addRental(new Rental(new Movie("<天外飞仙>", Movie.REGUALAR), 1));
+        customer.addRental(new Rental(new Movie("<爸爸去哪儿>", Movie.NEW_RELEASE), 1));
+
+        System.out.println(customer.statement());
+    }
 }
