102d4112c29f4906b6ea283c08f8923f73bdde46
Little <414929680@qq.com>
2021-03-28 17:06:28 +0800

    6.1 Extract Method (进一步优化)

--- app/src/test/java/com/zfang/graphicdemo/refact/Rental.java
+++ app/src/test/java/com/zfang/graphicdemo/refact/Rental.java
@@ -48,13 +48,10 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        // add frequent renter points
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            frequentRenterPoints++;
+            return 2;
+        } else {
+            return 1;
         }
-
-        return  frequentRenterPoints;
     }
 }
