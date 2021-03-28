5ec7a79d5ff437e893536366a87feac43aab9d89
Little <414929680@qq.com>
2021-03-28 17:29:13 +0800

    8.1 Move Method (去掉Rental中的getFrequentRenterPoints逻辑)

--- app/src/test/java/com/zfang/graphicdemo/refact/Movie.java
+++ app/src/test/java/com/zfang/graphicdemo/refact/Movie.java
@@ -53,4 +53,12 @@ public class Movie {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
