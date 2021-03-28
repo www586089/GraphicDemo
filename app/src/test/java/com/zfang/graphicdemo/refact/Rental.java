946a9a599ed847de97345c88ff6cb3019619ba88
Little <414929680@qq.com>
2021-03-28 16:47:33 +0800

    Move Method，把行为放到相应类中

--- app/src/test/java/com/zfang/graphicdemo/refact/Rental.java
+++ app/src/test/java/com/zfang/graphicdemo/refact/Rental.java
@@ -17,4 +17,33 @@ public class Rental {
     public int getDaysRented() {
         return _daysRented;
     }
+
+    /**
+     * 重构就是小步修改程序，如果犯错很容易发现他。
+     * @return
+     */
+    public double getCharge() {
+        double thisAmount = 0;
+        //determine amounts for each line
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGUALAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2) {
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3) {
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
 }
