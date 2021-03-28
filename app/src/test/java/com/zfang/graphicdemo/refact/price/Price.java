23c5dffa8fde57a2afdd3b5722156f941c19821a
Little <414929680@qq.com>
2021-03-28 20:42:35 +0800

    9.4 Movie Method, 把Movie中的getFrequentRenterPoints移到Price中

--- app/src/test/java/com/zfang/graphicdemo/refact/price/Price.java
+++ app/src/test/java/com/zfang/graphicdemo/refact/price/Price.java
@@ -1,6 +1,16 @@
 package com.zfang.graphicdemo.refact.price;
 
+import com.zfang.graphicdemo.refact.Movie;
+
 public abstract class Price {
     public abstract double getCharge(int daysRented);
     public abstract int getPriceCode();
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
